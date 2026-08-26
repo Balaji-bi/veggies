0cb51036bc813396e973c6b9404bf9909afd6136
Balaji-bi <balameanbi@gmail.com>
2026-08-26 17:13:14 +0530

    upfate for

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/veggies' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 module.exports = nextConfig;
